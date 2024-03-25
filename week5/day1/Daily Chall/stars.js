d769b2232573d9380084f676ee76851cefe94931
Dmitry Ashavskiy <ashavskydb@users.noreply.github.com>
2024-03-26 00:24:15 +0200

    add daily chall1 week5 day 1

---
+++ week5/day1/Daily Chall/stars.js
@@ -0,0 +1,24 @@
+// const maxNumber = 6;
+
+// // -- Single Loop  --
+
+// let starsSingleLoop = '';
+// for (let i = 1; i <= maxNumber; i++) {
+//     starsSingleLoop += '*'.repeat(i) + '\n';
+// }
+// console.log("Single Loop Approach:\n" + starsSingleLoop);
+
+// // -- Nested Loop --
+
+// function printPatternWithTwoLoops(rows) {
+//     let starsNestedLoop = '';
+//     for (let i = 1; i <= rows; i++) {
+//         for (let j = 1; j <= i; j++) {
+//             starsNestedLoop += '*';
+//         }
+//         starsNestedLoop += '\n';
+//     }
+//     console.log("Nested Loop Approach:\n" + starsNestedLoop);
+// }
+
+// printPatternWithTwoLoops(maxNumber);
