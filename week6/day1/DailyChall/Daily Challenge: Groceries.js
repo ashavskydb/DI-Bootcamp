3c43608a35c8f6f74e0f9aa9437b5b80b6e10fcf
Dmitry Ashavskiy <ashavskydb@users.noreply.github.com>
2024-04-01 10:37:32 +0300

    add week6 day1 Daily Chall

---
+++ week6/day1/DailyChall/Daily Challenge: Groceries.js
@@ -0,0 +1,39 @@
+let client = "John";
+
+const groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        paid : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+const displayGroceries = () => {
+    groceries.fruits.forEach(fruit => console.log(fruit));
+}
+
+const cloneGroceries = () => {
+    
+    let user = client;
+
+    
+    client = "Betty";
+
+   
+    let shopping = { ...groceries };
+
+    
+    shopping.totalPrice = "35$";
+
+    
+    shopping.other.paid = false;
+
+    
+    console.log("Modified shopping object:", shopping);
+}
+
+displayGroceries();
+
+cloneGroceries();
